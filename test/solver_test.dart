866c7159ab4a6930d939a0ca345d9db46f61f5ec
bhendee <85202956+bhendee@users.noreply.github.com>
2023-05-14 21:40:14 -0700

    Add tests for OT solver

--- test/solver_test.dart
+++ test/solver_test.dart
@@ -14,4 +14,27 @@ void main() {
         );
         expect(rankOT(tableaux), equals([{const Constraint('B')}, {const Constraint('A')}]));
     });
+
+    test('rankOT properly ranks three constraints', () {
+        List<Constraint> constraints = Constraint.fromStrings(['A', 'B', 'C']);
+        Tableaux tableaux = Tableaux(
+            constraints,
+            [
+                Tableau('a', constraints, ['aa', 'bb'], [[1, 0, 0], [0, 1, 0]], 'aa'),
+                Tableau('b', constraints, ['aa', 'bb'], [[1, 0, 1], [0, 0, 0]], 'bb'),
+            ]
+        );
+        expect(rankOT(tableaux), equals([{const Constraint('B'), const Constraint('C')}, {const Constraint('A')}]));
+    });
+    test('rankOT throws when no ranking exits', () {
+        List<Constraint> constraints = Constraint.fromStrings(['A', 'B']);
+        Tableaux tableaux = Tableaux(
+            constraints,
+            [
+                Tableau('a', constraints, ['aa', 'bb'], [[1, 0], [0, 0]], 'aa'),
+                Tableau('b', constraints, ['aa', 'bb'], [[1, 0], [0, 0]], 'bb'),
+            ]
+        );
+        expect(() => rankOT(tableaux), throwsFormatException);
+    });
 }
